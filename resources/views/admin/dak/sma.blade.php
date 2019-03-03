b8e66d638101aba0807e9c41aa69ff5be74e8494
Muhammad Alfian <muhammadalfian500@gmail.com>
2019-03-03 21:34:41 +0700

    update

--- resources/views/admin/dak/sma.blade.php
+++ resources/views/admin/dak/sma.blade.php
@@ -14,7 +14,7 @@
       <div class="container-fluid h-100">
         <div class="row">
           <div class="col-12">
-            <iframe width="100%" height="470px" src="https://onedrive.live.com/embed?cid=AD5A46866F5DBEEB&resid=AD5A46866F5DBEEB%21857&authkey=AJKobyzwAT-1U-s&em=2" frameborder="0" scrolling="no"></iframe>
+            <iframe width="100%" height="470px" src="https://docs.google.com/spreadsheets/d/1UpDxXjWOBr4ijV3p92qa7T4ukQVSb6JuV6a0pNdpNV0/edit#gid=1871147240" frameborder="0" scrolling="no"></iframe>
           </div>
         </div>
       </div>
