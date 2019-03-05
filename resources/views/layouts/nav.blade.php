bb66e82a485ac278020c26285d7b43043934b76d
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-03-06 04:15:47 +0700

    menghilangkan nav guest

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,12 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    @guest
+    @else
     <a class="navbar-brand" href="{{ url('/') }}">Dinas Pendidikan Provinsi Jawa Timur</a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    @guest
-    @else
+
     <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
