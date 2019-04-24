55f55c1a60eee56cafab28d97e9daa2057d9658f
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-04-24 13:32:57 +0700

    disable total connections

--- resources/views/report/dnsbydate.blade.php
+++ resources/views/report/dnsbydate.blade.php
@@ -56,7 +56,7 @@
 </div>
 
 <div class="row">
-  <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+  <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
     <div class="card card-statistic-1">
       <div class="card-icon bg-primary">
         <i class="far fa-user"></i>
@@ -70,8 +70,8 @@
         </div>
       </div>
     </div>
-  </div>
-  <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+  </div> -->
+  <div class="col-lg-3 col-md-6 col-sm-6 col-12">
     <div class="card card-statistic-1">
       <div class="card-icon bg-danger">
         <i class="far fa-newspaper"></i>
@@ -85,7 +85,7 @@
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
   <div class="col-lg-3 col-md-6 col-sm-6 col-12">
     <div class="card card-statistic-1">
       <div class="card-icon bg-success">
